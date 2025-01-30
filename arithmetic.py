fbd7645d97efe943b61ec9ddd3c23675eddfd41d
vismaygharat11 <vismaygharat11@gmail.com>
2025-01-30 15:13:23 +0530

    Multiplication and Division

--- arithmetic.py
+++ arithmetic.py
@@ -4,14 +4,6 @@ import pandas as pd
 a=1
 b=2
 
-#addition
-c=a+b
-print(f"Sum is {c}")
-
-#subtraction
-d=a-b
-print(f"Subtraction is {d}")
-
 #Multiplication
 e=a*b
 print(f"Multiplication is {e}")
